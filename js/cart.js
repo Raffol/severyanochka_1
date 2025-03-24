c2b657b22ca123211fb04f9df9ddc589c99af328
Raffol <eremenko97@bk.ru>
2025-03-24 10:19:58 +0800

    fix: сделал рабочий счётчик

--- js/cart.js
+++ js/cart.js
@@ -1,25 +1,15 @@
-const quantities = document.querySelectorAll('.item-cart__counter');
+//Счётчик
+let count = 0;
+document.getElementById("item-cart__counter-reduce").onclick = function(){
 
-[...quantities].forEach(function (quantity) {
-    const minusButton = quantity.querySelector('.item-cart__counter-reduce');
-    const plusButton = quantity.querySelector('.item-cart__counter-increase');
-    const inputField = quantity.querySelector('.item-cart__price');
+    count -=1;
+    document.getElementById("countLabel").innerHTML = count;
+}
+document.getElementById("item-cart__counter-increase").onclick = function(){
+    count +=1;
+    document.getElementById("countLabel").innerHTML = count;
+}
 
-    minusButton.addEventListener('click', function minusProduct() {
-       const currentValue = Number(inputField.value);
-        console.log(minusButton.value);
-       if (currentValue > 0){
-           inputField.value = currentValue - 1;
-       }else{
-           inputField.value = 0;
-       }
-
-    });
-    plusButton.addEventListener('click', function plusProduct(){
-        const currentValue = Number(inputField.value);
-        inputField.value = currentValue + 1;
-    });
-})
 
 //Тумблер списания
 document.getElementById('switch-btn').addEventListener('change', function () {
