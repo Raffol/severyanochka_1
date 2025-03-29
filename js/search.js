feded40d196f1a3ec7641e0a88f97fc6c859eb53
Raffol <eremenko97@bk.ru>
2025-03-29 15:06:26 +0800

    fix: сделал +- search.js

--- js/search.js
+++ js/search.js
@@ -17,8 +17,66 @@ const products = [
 ];
 
 // Сохранение текущего значения поля ввода в URL
+const updateURL = (searchTerm) => {
+    const params = new URLSearchParams(window.location.search);
+    if (searchTerm) {
+        params.set("search", searchTerm);
+    }else{
+        params.delete("search");
+    }
+    history.pushState({}, "", `${window.location.pathname}?${params.toString()}`);
+};
 
+//умный поиск
+const handleSearch = () => {
+    const searchTerm = searchField.value.trim();
+    updateURL(searchTerm);
 
+    const resultSearch = products.filter(
+        (product) =>
+            searchField !== "" &&
+            product.toLowerCase().includes(searchTerm.toLowerCase())
+    )
+};
+const regex = new RegExp(`(${searchTerm})`, "gi");
+if (searchResult.length){
+    form.classList.add("active");
+    searchList.innerHTML = "";
+    searchResult.filter((item)=>{
+        const highlitedItem = item.replace(
+            regex,
+            `<span class="highlight">$1</span>`
+        );
+        searchList.insertAdjacentHTML(
+            "beforeend",
+            `<li class="search__item">${highlitedItem}</li>`
+        );
+    });
+}else{
+    form.classList.remove("active");
+}
+
+//запись значений в поле ввода
+document.addEventListener("click", (event) => {
+    if (event.target.closest(".search__item")){
+        searchField.value = event.target.innerText;
+        form.classList.remove("active");
+    }
+});
+
+//для записи в поле ввода
+window.addEventListener("DOMContentLoaded", ()=> {
+    const params = new URLSearchParams(window.location.search);
+    const searchTerm = params.get("search");
+
+    if (searchTerm) {
+        searchField.value = searchTerm;
+        handleSearch();
+    }
+});
+
+//при вводе в поле ввода поиска
+searchField.addEventListener("input", handleSearch);
 
 
 
